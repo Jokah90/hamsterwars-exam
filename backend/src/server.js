16f198b854a6920a8800a4b32356da9691a0a737
John Hansson <john@MacBook-Pro-som-tillhor-John.local>
2021-11-01 10:28:17 +0100

    debugging server

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,4 +1,5 @@
 // importera paket
+console.log('1')
 const express = require('express')
 const app = express()
 const router = require('./routes/router.js')
@@ -6,16 +7,19 @@ const routerMatches = require('./routes/routesMatches.js')
 const cors = require('cors')
 
 let count = 0;
+console.log('2')
 
 // konfigurera
 const PORT = process.env.PORT || 1337;
 app.use(cors());
+console.log('3')
 
 // middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use( express.static(__dirname + '/../../build') ) 
 // Lägg till map för bilderna KOM IHÅG
+console.log('4')
 
 
 //Logger
@@ -25,17 +29,22 @@ app.use((req, res, next) => {
 
   next();
 });
+console.log('5')
 
 
 
 // routes / endpoints
 app.use('/hamsters', router)
+console.log('6')
+
 app.use('/matches', routerMatches);
+console.log('7')
 
 
 app.get('*', (req, res) => {
 	res.sendFile(__dirname + '/../../build/index.html')
 })
+console.log('8')
 
 // starta servern
 app.listen(PORT, () => {
