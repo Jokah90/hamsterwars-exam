65a2de4558ad6e8bb13722a734e145ec1e5ecf20
John Hansson <johnhansson90@gmail.com>
2021-12-10 21:43:42 +0100

    rearranged img

--- backend/src/server.js
+++ backend/src/server.js
@@ -28,7 +28,7 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use('/img', express.static(__dirname + '/images/hamsters'));
+// app.use('/img', express.static(__dirname + '/images/hamsters'));
 
 
 // routes / endpoints
